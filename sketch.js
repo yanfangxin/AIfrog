9109b5fbfbed6d46bcdf9729991b9f9e7db7c657
yanfangxin <113405762+yanfangxin@users.noreply.github.com>
2023-12-25 11:58:40 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -28,7 +28,6 @@ function setup() {
   createCanvas(320, 240);
   // Start classifying
   // The sound model will continuously listen to the microphone
-  ThunkableWebviewerExtension.postMessage("ready");
   classifier.classify(gotResult);
 }
 
@@ -51,5 +50,4 @@ function gotResult(error, results) {
   // The results are in an array ordered by confidence.
   // console.log(results[0]);
   label = results[0].label;
-  ThunkableWebviewerExtension.postMessage(label);
 }
